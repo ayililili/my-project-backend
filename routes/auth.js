490c091cc39b110ab9a6a75cea47cf29bfed0ed7
ayililili <134775797+ayililili@users.noreply.github.com>
2024-06-26 20:04:33 +0800

    refreshToken api

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,9 @@ const router = express.Router();
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken');
 
+const authenticateJWT = require('./middlewares/authenticateJWT');
 const User = require('../modules/User');
+const RefreshToken = require('../modules/RefreshToken');
 
 router.post('/register', async (req, res) => {
   const { username, password, email } = req.body;
@@ -49,14 +51,77 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid password' });
     }
 
-    const token = jwt.sign(
+    const accessToken = jwt.sign(
       { id: user._id, username: user.username },
       process.env.SECRET_KEY,
-      { expiresIn: '1m' }
+      { expiresIn: '20m' }
     );
-    res.json({ token });
+    const refreshToken = jwt.sign(
+      { id: user._id, username: user.username },
+      process.env.REFRESH_SECRET_KEY,
+    )
+
+    await RefreshToken.create({
+      userId: user._id,
+      token: refreshToken
+    });
+
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      secure: true
+    })
+    res.json({ accessToken });
   } catch (error) {
     res.status(500).json({ error: 'An error occurred during login' });
+  }
+});
+
+router.post('/logout', authenticateJWT, async (req, res) => {
+  try {
+    const token = req.cookies.refreshToken;
+    if (!token) {
+      return res.status(400).json({ message: 'Refresh token not found' });
+    }
+
+    const savedToken = await RefreshToken.findOne({ token });
+    if (!savedToken) {
+      return res.status(400).json({ error: 'Invalid refresh token' });
+    }
+
+    await RefreshToken.deleteOne({ token });
+    res.clearCookie('refreshToken').json({ message: 'Account has been logged out'});
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred during logout' });
+  }
+});
+
+router.post('/token', async (req, res) => {
+  try {
+    const token = req.cookies.refreshToken;
+    if (!token) {
+      return res.status(400).json({ error: 'Refresh token not found' });
+    }
+
+    const savedToken = await RefreshToken.findOne({ token });
+    if (!savedToken) {
+      return res.status(400).json({ error: 'Invalid refresh token' });
+    }
+
+    jwt.verify(token, process.env.REFRESH_SECRET_KEY, (err, user) => {
+      if (err) {
+        return res.status(403).json({ error: 'Invalid refresh token' });
+      }
+
+      const accessToken = jwt.sign(
+        { id: user.id, username: user.username },
+        process.env.SECRET_KEY,
+        { expiresIn: '20m' }
+      );
+      res.json({ accessToken });
+    });
+  } catch (error) {
+    console.error('Error during token refresh:', error);
+    res.status(500).json({ error: 'An error occurred during token refresh' });
   }
 });
 
